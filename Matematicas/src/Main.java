9d8e1c802937fa1347795652426b2625465f7bde
Gabriel Santiago <gabosanti2003@gmail.com>
2024-06-27 19:14:27 -0500

    update

--- Matematicas/src/Main.java
+++ Matematicas/src/Main.java
@@ -30,6 +30,11 @@ public class Main {
         double logaritmo1 = Math.log(num1);
         double logaritmo2 = Math.log(num2);
 
+        // Resultados
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
 
 
 
