bc8e837415a6fa4d7cbf366e154295ecf84d92c9
Gabriel Santiago <gabosanti2003@gmail.com>
2024-06-27 19:13:02 -0500

    update

--- Matematicas/src/Main.java
+++ Matematicas/src/Main.java
@@ -10,6 +10,14 @@ public class Main {
         System.out.println("Ingrese el segundo número:");
         double num2 = scanner.nextDouble();
 
+        // Operaciones básicas
+        double suma = num1 + num2;
+        double resta = num1 - num2;
+        double multiplicacion = num1 * num2;
+        double division = num1 / num2;
+        double modulo = num1 % num2;
+
+
 
 
         scanner.close();
